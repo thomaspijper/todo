a40086625995f47e98235f20e3cfc0bacc8e3d2d
Thomas Pijper <19342403+thomaspijper@users.noreply.github.com>
2025-01-24 23:22:13 +0100

    New feature: multiple undoes (#9)

--- src/file_io.rs
+++ src/file_io.rs
@@ -24,11 +24,11 @@ impl error::Error for FileError { }
 impl fmt::Display for FileError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            FileError::Deserialize(e) => writeln!(f, "Unable to deserialize save file contents. Details:\n    {}", e),
-            FileError::Serialize(e) => writeln!(f, "Unable to serialize data for saving. Details:\n    {}", e),
-            FileError::Io(e) => writeln!(f, "Unable to save data. Details:\n    {}", e),
-            FileError::CreateDir(e) => writeln!(f, "Unable to create directory for saving data. Details:\n    {}", e),
-            FileError::BackupMissing => writeln!(f, "Unable to undo. The backup file does not exist")
+            FileError::Deserialize(e) => write!(f, "Unable to deserialize save file contents. Details:\n    {}", e),
+            FileError::Serialize(e) => write!(f, "Unable to serialize data for saving. Details:\n    {}", e),
+            FileError::Io(e) => write!(f, "Unable to save data. Details:\n    {}", e),
+            FileError::CreateDir(e) => write!(f, "Unable to create directory for saving data. Details:\n    {}", e),
+            FileError::BackupMissing => write!(f, "Unable to undo. No undos are available")
         }
     }
 }
@@ -39,7 +39,7 @@ pub fn get_filename() -> PathBuf {
 
     let directory = data_dir();
     if directory.is_none() {
-        println!("Error: could not discern the user's data directory. Exiting...\n")
+        println!("Error: could not find the user's data directory. Exiting...\n")
     }
     
     let mut filename = directory.unwrap();
@@ -73,28 +73,72 @@ pub fn save_file(filename: &PathBuf, tasks: &Vec<Task>) -> Result<()> {
         println!("Creating tasks file: {:?}\n", filename)
     };
 
-    // Create backup file for undo operation
-    let mut filename_backup = PathBuf::from(filename);
-    filename_backup.set_extension("old");
-    if filename.exists() {
-        rename(filename, filename_backup).map_err(FileError::Io)?;
-    }
+    // Create a backup
+    create_backup(filename)?;
 
+    // Save the file
     let mut file = File::create(filename).map_err(FileError::Io)?;
     write!(file, "{data_json}").map_err(FileError::Io)?;
 
     Ok(())
 }
 
-// Undo last operation by rolling back file
-pub fn roll_back_file(filename: &PathBuf) -> Result<()> {
-    let mut filename_backup = PathBuf::from(filename);
-    filename_backup.set_extension("old");
+// Create a backup file for undo. Maximum number of backup files is 10. The newest file 
+// has extension ".000", the oldest extension ".010". 
+fn create_backup(filename: &PathBuf) -> Result<()> {
+    let max_undos = 10;
 
-    if filename_backup.exists() {
-        rename(filename_backup, filename).map_err(FileError::Io)?;
-        Ok(())
-    } else {
-        Err(FileError::BackupMissing)
+    // Rename all existing backup files
+    for i in (0..max_undos).rev() {
+        let mut backup_older = PathBuf::from(filename);
+        backup_older.set_extension( format!("{:03}", i+1) );
+
+        let mut backup_newer = PathBuf::from(filename);
+        backup_newer.set_extension( format!("{:03}", i) );
+
+        if backup_newer.exists() {
+            rename(backup_newer, backup_older).map_err(FileError::Io)?;
+            println!("renamed");
+        }
     }
+
+    // Create newest backup file
+    let mut backup_newest = PathBuf::from(filename);
+    backup_newest.set_extension("000");
+    if filename.exists() {
+        rename(filename, backup_newest).map_err(FileError::Io)?;
+    }
+
+    Ok(())
+}
+
+// Undo last operation by rolling back files
+pub fn roll_back_file(filename: &PathBuf) -> Result<()> {
+    let max_undos = 10;
+
+    for i in 0..max_undos+1 {
+        if i == 0 {
+            // Restore newest backup file
+            let mut backup_newest = PathBuf::from(filename);
+            backup_newest.set_extension( format!("{:03}", i) );
+            if backup_newest.exists() {
+                rename(backup_newest, filename).map_err(FileError::Io)?;
+            } else {
+                return Err(FileError::BackupMissing);
+            }
+        } else {
+            // Rename older backup files
+            let mut backup_older = PathBuf::from(filename);
+            backup_older.set_extension( format!("{:03}", i) );
+    
+            let mut backup_newer = PathBuf::from(filename);
+            backup_newer.set_extension( format!("{:03}", i - 1) );
+
+            if backup_older.exists() {
+                rename(backup_older, backup_newer).map_err(FileError::Io)?;
+            }
+        }   
+    }
+
+    Ok(())
 }
